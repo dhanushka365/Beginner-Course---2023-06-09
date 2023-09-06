a8fe1e544d7d842029ec3763bf89bdc28edac8f8
dhnaushka365 <pasindudhanushka365@gmail.com>
2023-09-06 06:36:33 +0530

    Add project files.

---
+++ Beginner Course - 2023-06-09/Program.cs
@@ -0,0 +1,18 @@
+using Microsoft.AspNetCore.Builder;
+
+
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+app.Use( async(context,next) =>
+    {
+    if (context.Request.Method == HttpMethods.Get && context.Request.Query["custom"]=="true")
+    {
+            context.Response.ContentType = "text/plain";
+            await context.Response.WriteAsync("Custom Middleware \n");
+    }
+
+        await next();
+});
+app.MapGet("/", () => "Hello World!");
+
+app.Run();
